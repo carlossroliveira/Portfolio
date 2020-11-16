69547a613423557aa323d6bf540376a15f77fa0e
carlossroliveira <63623377+carlossroliveira@users.noreply.github.com>
2020-11-15 21:51:16 -0300

    at
    15/11/2020

--- app/sticky.js
+++ app/sticky.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', () => {
     let pegaHeader = document.querySelector('.sectionP');
-    let windowPosition = window.scrollY > 300;
+    let windowPosition = window.scrollY > 100;
     pegaHeader.classList.toggle('tActive', windowPosition);
 });
